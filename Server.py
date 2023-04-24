96247748dd5a76de07ba832bbb2e336bc931d06e
Jason Salem <44297233+jasonsalem2002@users.noreply.github.com>
2023-04-24 13:16:55 +0300

    Update of Server.py
    Old version was developed without the client and contained many problems. This version is coded again from scratch in parallel with the clients.
    Working features:
    
    1 - Accept only two connections and reject the third one
    2-  Generate random number and send it to both connections using a for loop. Connection 1 receive the message first. Need to work on this point to send the message at the same time.

--- Server.py
+++ Server.py
@@ -1,167 +1,53 @@
-import socket
-import time
-from random import random
+import socket, time, random
 
 
-def get_num_players(conn, addr):
-    conn.send("Please enter the number of players (2-5): ".encode())
-    num_players = conn.recv(1024).decode().strip()
-    while not num_players.isdigit() or int(num_players) < 2 or int(num_players) > 5:
-        conn.send("Invalid input. Please enter a number between 2 and 5: ".encode())
-        num_players = conn.recv(1024).decode().strip()
-    print(f"{addr} has chosen to play with {num_players} players.")
-    return int(num_players)
+def random_number_generator():
+    random.seed(time.time())
+    return random.randint(0, 9)
 
 
-def get_username(conn, addr, player_num):
-    conn.send(f"Please enter your username for Player {player_num}: ".encode())
-    username = conn.recv(1024).decode().strip()
-    while not username:
-        conn.send("Invalid input. Please enter a username: ".encode())
-        username = conn.recv(1024).decode().strip()
-    print(f"{addr} has chosen the username '{username}' for Player {player_num}.")
-    return username
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+host = socket.gethostname()
+port = 12345
+server_socket.bind((host, port))
+server_socket.listen(2)
+print("Server is listening for incoming connections...")
+connections = 0
+usernames = []
+clients = []
 
-def announce_new_player(conn_list, new_player_username):
-    for conn in conn_list:
-        conn.send(f"{new_player_username} has joined the game.".encode())
+while True:
 
+    if connections >= 2:
+        conn3, addr3 = server_socket.accept()
+        conn3.send("Sorry server is at capacity".encode())
+        conn3.close()
+        print(f"Connection refused for {addr3}")
+        continue
 
-def send_countdown(conn_list):
-    for conn in conn_list:
-        conn.send(f"The game will start in 3 seconds.".encode())
-        time.sleep(1)
+    if connections == 0:
+        conn1, addr1 = server_socket.accept()
+        clients.append(conn1)
+        connections += 1
+        conn1.send("Please enter a username: ".encode())
+        username1 = conn1.recv(1024).decode().strip()
+        usernames.append(username1)
+        conn1.send(f"Welcome {usernames[0]}! Kindly wait for another player to join the game.".encode())
 
-    for i in range(3, 0, -1):
-        for conn in conn_list:
-            conn.send(f"{i}...".encode())
-            time.sleep(1)
+        conn2, addr2 = server_socket.accept()
+        clients.append(conn2)
+        connections += 1
+        conn2.send("Please enter a username: ".encode())
+        username2 = conn2.recv(1024).decode().strip()
+        usernames.append(username2)
+        conn2.send(f"Welcome {usernames[1]}! You will be playing against {usernames[0]}.".encode())
+        conn1.send(f"{usernames[1]} joined the game!".encode())
 
+        print(f"Connected with {usernames[0]} on " + str(addr1) + f" and {usernames[1]} on " + str(addr2))
 
-def run_server():
-    host = ""
-    port = 9999
-    backlog = 5
-    player_num = 1
-    ready_players = 0
+        number = random_number_generator()
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen(backlog)
-        print(f"Server listening on {host}:{port}...")
-        conn_list = []
-        num_rounds = 3
-        final_scores = {}
-        num_players = 0
-        while True:
-            conn, addr = s.accept()
-            print(f"Connected by {addr}")
-            conn_list.append(conn)
+        for client in clients:
+            client.sendall(f"Number is: {number}".encode())
 
-            if num_players == 0:
-                num_players = get_num_players(conn, addr)
-                conn.send(f"You are Player {player_num}. There will be {num_players} players in this game.".encode())
-            else:
-                username = get_username(conn, addr, player_num)
-                announce_new_player(conn_list, username)
-                conn.send(f"You are Player {player_num}. There will be {num_players} players in this game.".encode())
-
-            player_num += 1
-            if player_num > num_players:
-                break
-
-        print(f"All players connected. Game starting with {num_players} players.")
-
-        for round_num in range(1, num_rounds + 1):
-
-            for conn in conn_list:
-                conn.send(
-                    f"Game starting with {num_players} players. Send 'ready' when you're ready to start.".encode())
-
-            while ready_players < num_players:
-                conn, addr = s.accept()
-                message = conn.recv(1024).decode().strip()
-                if message.lower() == "ready":
-                    ready_players += 1
-
-            print(f"All players are ready.")
-
-            print(f"Round {round_num} starting...")
-
-            send_countdown(conn_list)
-
-            random.seed(time.time_ns())
-            number = random.randint(0, 9)
-
-            # send the number to all clients
-            for conn in conn_list:
-                conn.send(f"The number is: {number}".encode())
-
-            # start the timer for all clients
-            start_time = time.time()
-            rtt_list = []
-            while time.time() - start_time < 3:
-                # receive the numbers entered by clients and check if they match the sent number
-                for conn in conn_list:
-                    try:
-                        received_data = conn.recv(1024).decode().strip()
-                        if int(received_data) != number:
-                            conn.close()
-                            conn_list.remove(conn)
-                        else:
-                            rtt = time.time() - start_time
-                            rtt_list.append((conn.getpeername()[0], rtt))
-                    except:
-                        pass
-
-            # sort the RTT list in ascending order
-            sorted_rtt = sorted(rtt_list)
-
-            # determine the winner
-            winner_rtt = sorted_rtt[0]
-            winner_index = rtt_list.index(winner_rtt)
-            winner_conn = conn_list[winner_index]
-
-            for rtt in rtt_list:
-                conn_ip = rtt[0]
-                rtt_time = rtt[1]
-                if conn_ip not in final_scores:
-                    final_scores[conn_ip] = [rtt_time]
-                else:
-                    final_scores[conn_ip].append(rtt_time)
-
-                # existing code to clear the connection list and ready players for the next round
-            conn_list.clear()
-            ready_players = 0
-
-    final_scores_list = []
-    for conn_ip, scores in final_scores.items():
-        total_score = sum(scores)
-        final_scores_list.append((conn_ip, total_score))
-    final_scores_list = sorted(final_scores_list, key=lambda x: x[1])
-
-    # determine the final winner
-    winner_conn_ip = final_scores_list[0][0]
-    winner_score = final_scores_list[0][1]
-    winner_conn = None
-    for conn in conn_list:
-        if conn.getpeername()[0] == winner_conn_ip:
-            winner_conn = conn
-
-    # send message to the final winner
-    winner_message = f"Congratulations! You won the game with a total score of {winner_score:.3f} seconds!"
-    winner_conn.send(winner_message.encode())
-
-    # send message to other players with their total scores and ranks
-    for i, conn in enumerate(conn_list):
-        if conn.getpeername()[0] != winner_conn_ip:
-            conn_ip = conn.getpeername()[0]
-            total_score = sum(final_scores[conn_ip])
-            rank = final_scores_list.index((conn_ip, total_score)) + 1
-            message = f"You finished in {rank} place with a total score of {total_score:.3f} seconds."
-            conn.send(message.encode())
-
-
-if __name__ == "__main__":
-    run_server()
