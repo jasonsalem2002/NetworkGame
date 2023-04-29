4dc3503e93a0f0db08068a66d332c16eb69907cd
Jason Salem <44297233+jasonsalem2002@users.noreply.github.com>
2023-04-29 19:59:13 +0300

    Update Client1.py

--- Client1.py
+++ Client1.py
@@ -1,42 +1,66 @@
-import socket, time, threading
+import socket
 
-# create socket object
+host = '127.0.0.1'  # Localhost
+port = 8080
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# get local machine name
-host = socket.gethostname()
-
-# connect to the server
-port = 12345
 client_socket.connect((host, port))
+print("Connected to the server.")
 
-def get_user_input():
-    while True:
-        message = input()
-        client_socket.send(message.encode())
-
-def receive_messages():
-    while True:
-        mes = client_socket.recv(1024).decode()
-        print(mes)
-
-i = 0
 while True:
-    if i == 0:
-        warning = client_socket.recv(1024).decode()
 
-        if warning == "Sorry server is at capacity":
-            print(warning)
-            client_socket.close()
+    prompt = client_socket.recv(1024).decode()
+
+    if prompt == "Maximum number of connections reached. Disconnecting.":
+        print(prompt, end=' ')
+        client_socket.close()
+        break
+
+    if prompt == "Enter the maximum number of connections allowed: ":
+        print(prompt, end=' ')
+        max_connections = int(input())
+        client_socket.send(str(max_connections).encode())
+        prompt_username0 = client_socket.recv(1024).decode()
+        print(prompt_username0, end=' ')
+        user0 = input()
+        client_socket.send(user0.encode())
+        if user0:
+            while True:
+                ready = client_socket.recv(1024).decode()
+                if ready == "Game will soon begin!":
+                    print(ready)
+                    while True:
+                        countdown = client_socket.recv(1024).decode()
+                        print(countdown)
+                        if "Number" in countdown:
+                            send_number = int(input("Enter the number: "))
+                            client_socket.send(str(send_number).encode())
+                    client_socket.close()
+                    break
+                print(ready, end=' ')
+                is_ready = input()
+                client_socket.send(is_ready.encode())
             break
-        else:
-            username = input(warning)
-            client_socket.send(str(username).encode())
-            i += 1
-            # Start the user input thread
-            user_input_thread = threading.Thread(target=get_user_input)
-            user_input_thread.start()
-            # Start the message receiving thread
-            receive_thread = threading.Thread(target=receive_messages)
-            receive_thread.start()
-            continue
+
+    elif prompt == "Please enter your username: ":
+        print(prompt, end=' ')
+        user = input()
+        client_socket.send(user.encode())
+        if user:
+            while True:
+                ready = client_socket.recv(1024).decode()
+                if ready == "Game will soon begin!":
+                    print(ready)
+                    while True:
+                        countdown = client_socket.recv(1024).decode()
+                        print(countdown)
+                        if "Number" in countdown:
+                            send_number = int(input("Enter the number: "))
+                            client_socket.send(str(send_number).encode())
+                    client_socket.close()
+                    break
+                print(ready, end=' ')
+                is_ready = input()
+                client_socket.send(is_ready.encode())
+            break
+
+
